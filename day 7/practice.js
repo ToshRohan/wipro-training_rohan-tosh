6814be116f55ac1fe44088daa101961cf87bfb93
Rohan Tosh <rohantosh3@gmail.com>
2026-05-08 17:05:35 +0530

    new task

---
+++ day 7/practice.js
@@ -0,0 +1,99 @@
+// let a=1;
+// let b=2;
+// let arr=[23,34,"i hate everyone"];
+// let z=NULL
+// console.log(typeof z);
+
+
+
+
+
+// let c=30;
+// let f= 3/5*c+32;
+// console.log(f);
+
+
+
+
+
+
+// const age=(year)=>{
+//     // return 2026-year;
+//     console.log(2026-year);
+// }
+
+// age(2003);
+
+
+
+
+
+// let str1= "Hi I am a human";
+// let str2="";
+// for (let i=str1.length-1;i>=0;i--){
+   
+//    str2=str2+str1[i];
+// }
+
+// console.log(str2);
+
+
+
+
+// let str="i am a good boy";
+// let j=0;
+// for(let i=0;i<str.length;i++){
+//     if (str[i]!=" ")
+//         {
+//             j++;
+//             continue;
+//         }
+// }
+// console.log(j);
+
+
+// let str="i am a good boy";
+// let j=0;
+// for(let i=0;i<str.length;i++){
+//     if (str[i]=="a" || str[i]=="e"||str[i]=="i"||str[i]=="o"||str[i]=="u")
+//         {
+//             j++;
+//             // continue;
+//         }
+// }
+// console.log(j);
+
+
+
+
+// let str="I am a bad boy";
+// new_str="";
+// for (let i=0;i<str.length;i++){
+//     if(i==0||str[i-1]==" "){
+//         new_str=new_str+str[i].toUpperCase();
+//     }else{
+//         new_str=new_str+str[i];
+//     }
+                    
+             
+// }
+// console.log(new_str);
+
+
+
+
+// 
+
+
+// let str = "I am a bad boy but with good heart";
+// let longest_word="";
+
+// let words = str.split(" ");
+// // console.log(words.length);
+
+// for (let i =0;i<words.length;i++){
+//     if (words[i].length>longest_word.length){
+//         longest_word=words[i];
+//     }
+// }
+// console.log(`The longest word in the sentence is: ${longest_word}`);
