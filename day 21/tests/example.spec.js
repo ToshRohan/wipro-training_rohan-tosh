1f74fb9c38e2614adc9b4ed2d2756ec4e81a8cfb
Rohan Tosh <rohantosh3@gmail.com>
2026-05-28 10:13:33 +0530

    yml file updated

--- day 21/tests/example.spec.js
+++ day 21/tests/example.spec.js
@@ -1,8 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-import { log } from 'node:console';
-
 test.describe('jio mart test', () => {
   test.use({ storageState: 'auth.json' });
   test.skip('search for fruits', async ({ page }) => {
@@ -16,13 +14,54 @@ test.describe('jio mart test', () => {
     await expect(page.getByText('fruits').first()).toBeVisible();
   });
 
-  test('profile', async ({ page }) => {
+  test.skip('profile', async ({ page }) => {
     await page.goto('https://www.jiomart.com/profile');
 
     const profileName = page.getByTestId("JDSText-text").nth(0);
     console.log(await profileName.textContent());
-    await expect(profileName).toHaveText("Rohan Tosh");
+    expect(profileName).toHaveText("Aaryan kumar");
   });
 });
 
+const searchData = [
+  { searchTerm: 'Playwright', expectedTitle: 'Playwright' },
+  { searchTerm: 'Testing', expectedTitle: 'Test' },
+  { searchTerm: 'Automation', expectedTitle: 'Automation' },
+  { searchTerm: 'Automation', expectedTitle: 'Automation' },
+];
+
+test.describe('paramertized test', () => {
+  test.beforeEach(async ({ page }) => await page.goto('https://en.wikipedia.org/wiki/Main_Page'));
+  for (const item of searchData) {
+    test(`search for ${item.searchTerm}`, async ({ page }) => {
+      await page.fill('input[name="search"]', item.searchTerm);
+      await page.keyboard.press('Enter');
+
+      await expect(page).toHaveTitle(new RegExp(item.expectedTitle, 'i'));
+    })
+  }
+});
+
+test.describe('soft assertions', () => {
+  test('soft', async ({ page }) => {
+    await page.goto("https://example.com/");
+
+    // line fail -> go to new line
+    await expect.soft(page.locator('h1')).toHaveText('Example Domain1');
+    await expect.soft(page.locator('p').first()).toBeVisible();
+    console.log("hello");
+  });
+});
+
+test('polling assertions', async ({ page }) => {
+  let counter = 0;
+
+  await expect.poll(async () => {
+    counter++;
+    return counter;
+  }, {
+    message: 'Counter did not reach 5',
+    timeout: 5000,
+  }).toBe(5);
+})
 
